faa3a5cabc87835dd13539a27c0e35fcf9413a39
YacoAng <tangeleri@frba.utn.edu.ar>
2025-04-06 20:37:17 +0000

    tp0 terminado

--- client/src/client.c
+++ client/src/client.c
@@ -3,7 +3,6 @@
 int main(void)
 {
 	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
-
 	int conexion;
 	char* ip;
 	char* puerto;
@@ -13,25 +12,24 @@ int main(void)
 	t_config* config;
 
 	/* ---------------- LOGGING ---------------- */
-
 	logger = iniciar_logger();
-
-	// Usando el logger creado previamente
-	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Hola soy un logger");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
-
 	config = iniciar_config();
 
-	// Usando el config creado previamente, leemos los valores del config y los 
-	// dejamos en las variables 'ip', 'puerto' y 'valor'
-
+	/* Usando el config creado previamente, leemos los valores del config y los dejamos 
+	en las variables 'ip', 'puerto' y 'valor'*/
+	valor = config_get_string_value(config, "CLAVE");
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	
 	// Loggeamos el valor de config
-
-
+	log_info(logger,"Valor: %s", valor);
+	log_info(logger, "IP: %s", ip);
+	log_info(logger,"Puerto: %s", puerto);
+	
 	/* ---------------- LEER DE CONSOLA ---------------- */
-
 	leer_consola(logger);
 
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
@@ -42,7 +40,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-
+	enviar_mensaje(valor, conexion);
 	// Armamos y enviamos el paquete
 	paquete(conexion);
 
@@ -54,15 +52,25 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log", "cliente", true, LOG_LEVEL_INFO);
+
+	if(nuevo_logger == NULL){
+		printf("No se pudo crear el logger\n");
+        abort(); //Mas abrupto que Exit(1)
+	}
 
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create ("cliente.config");
 
+	if (nuevo_config == NULL) {
+        printf("No se pudo abrir el archivo cliente.config");
+        exit(1); // No termina el programa abruptamente como abort()
+    }
+	
 	return nuevo_config;
 }
 
@@ -70,15 +78,16 @@ void leer_consola(t_log* logger)
 {
 	char* leido;
 
-	// La primera te la dejo de yapa
-	leido = readline("> ");
+	while (strcmp(leido, "") != 0) {
+            free(leido);
+			leido = readline(">");
+			log_info(logger, "%s",leido);
+            break;
+        }
 
-	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+        free(leido);  // readline usa malloc, hay que liberar la memoria
+    }
 
-
-	// ¡No te olvides de liberar las lineas antes de regresar!
-
-}
 
 void paquete(int conexion)
 {
@@ -87,9 +96,13 @@ void paquete(int conexion)
 	t_paquete* paquete;
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
+	leido = readline("Enviar mensaje al servidor: ");
+	agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+	enviar_paquete(paquete, conexion);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	eliminar_paquete(paquete);
+	free(leido);
 	
 }
 
@@ -97,4 +110,7 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  liberar_conexion(conexion);
+	  log_destroy(logger);
+	  config_destroy(config);
 }
--- client/src/utils.c
+++ client/src/utils.c
@@ -30,9 +30,15 @@ int crear_conexion(char *ip, char* puerto)
 
 	// Ahora vamos a crear el socket.
 	int socket_cliente = 0;
+	socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 
 	// Ahora que tenemos el socket, vamos a conectarlo
+    connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
 
+	 if (connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) != 0) {
+        perror("No se pudo conectar con el servidor");
+        exit(1);
+    }
 
 	freeaddrinfo(server_info);
 
